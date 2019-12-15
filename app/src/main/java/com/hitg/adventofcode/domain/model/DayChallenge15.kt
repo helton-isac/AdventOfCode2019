e631b502eae22638bb1d2d17573ac1ea76fd361a
Helton Isac <helton.isac@gmail.com>
2019-12-15 20:59:26 -0300

    Updating Inputs

--- app/src/main/java/com/hitg/adventofcode/domain/model/DayChallenge15.kt
+++ app/src/main/java/com/hitg/adventofcode/domain/model/DayChallenge15.kt
@@ -3,5 +3,5 @@ package com.hitg.adventofcode.domain.model
 class DayChallenge15 : DayChallenge {
     override fun getDay(): Int = 15
 
-    override fun getTitle(): String = "???"
+    override fun getTitle(): String = "Oxygen System"
 }
