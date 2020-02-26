aa3dd5787df563ba0a259e984d8571a2e4f115fc
Helton Isac <helton.isac@gmail.com>
2020-02-25 21:51:43 -0300

    Commenting IntCode Functions

--- app/src/main/java/com/hitg/adventofcode/domain/model/computer/IntCodeComputer.kt
+++ app/src/main/java/com/hitg/adventofcode/domain/model/computer/IntCodeComputer.kt
@@ -23,6 +23,13 @@ class IntCodeComputer(var input: Int? = null) {
 
     var outputResult: String = ""
 
+    /**
+     * Opcode 1 adds together numbers read from two positions and stores the result in a
+     * third position.
+     * The three integers immediately after the opcode tell you these three positions -
+     * the first two indicate the positions from which you should read the input values,
+     * and the third indicates the position at which the output should be stored.
+     */
     private fun adds(
         program: MutableList<String>,
         parametersMode: List<Int>,
@@ -35,6 +42,12 @@ class IntCodeComputer(var input: Int? = null) {
         program[positionToStore] = (firstValue + secondValue).toString()
     }
 
+    /**
+     * Opcode 2 works exactly like opcode 1,
+     * except it multiplies the two inputs instead of adding them.
+     * Again, the three integers after the opcode indicate where the inputs and outputs are,
+     * not their values.
+     */
     private fun multiplies(
         program: MutableList<String>,
         parametersMode: List<Int>,
@@ -47,16 +60,30 @@ class IntCodeComputer(var input: Int? = null) {
         program[positionToStore] = (firstValue * secondValue).toString()
     }
 
+    /**
+     * Opcode 4 outputs the value of its only parameter.
+     * For example, the instruction 4,50 would output the value at address 50.
+     */
     private fun output(program: MutableList<String>, parametersMode: List<Int>, position: Int) {
 
         outputResult =
             getParameterValue(program, parametersMode, 0, position).toString()
     }
 
+    /**
+     * Opcode 3 takes a single integer as input and saves it to the position given by its
+     * only parameter.
+     * For example, the instruction 3,50 would take an input value and store it at address 50.
+     */
     private fun input(program: MutableList<String>, position: Int) {
         program[position] = input.toString()
     }
 
+    /**
+     * Opcode 5 is jump-if-true:
+     * if the first parameter is non-zero, it sets the instruction pointer to the value
+     * from the second parameter. Otherwise, it does nothing.
+     */
     private fun jumpIfTrue(
         program: MutableList<String>,
         parametersMode: List<Int>,
@@ -73,6 +100,11 @@ class IntCodeComputer(var input: Int? = null) {
         }
     }
 
+    /**
+     * Opcode 6 is jump-if-false:
+     * if the first parameter is zero, it sets the instruction pointer to the value
+     * from the second parameter. Otherwise, it does nothing.
+     */
     private fun jumpIfFalse(
         program: MutableList<String>,
         parametersMode: List<Int>,
@@ -89,6 +121,12 @@ class IntCodeComputer(var input: Int? = null) {
         }
     }
 
+    /**
+     * Opcode 7 is less than:
+     * if the first parameter is less than the second parameter,
+     * it stores 1 in the position given by the third parameter.
+     * Otherwise, it stores 0.
+     */
     private fun lessThan(
         program: MutableList<String>,
         parametersMode: List<Int>,
@@ -105,6 +143,12 @@ class IntCodeComputer(var input: Int? = null) {
         }).toString()
     }
 
+    /**
+     * Opcode 8 is equals:
+     * if the first parameter is equal to the second parameter,
+     * it stores 1 in the position given by the third parameter.
+     * Otherwise, it stores 0.
+     */
     private fun equal(
         program: MutableList<String>,
         parametersMode: List<Int>,
